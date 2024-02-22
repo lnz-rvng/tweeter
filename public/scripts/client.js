c7e4bd16c9e95eaa54156d3ff4ba08cf2fb9760d
Lance <lanceirvingmanzanal@icloud.com>
2024-02-21 23:31:53 -0700

    Update the client.js file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -52,26 +52,7 @@ $(document).ready(() => {
     }
   }
 
-  const charCounter = function () {
-    const maxLength = 140;
-    const length = $(this).val().length;
-    const remainingChar = maxLength - length;
-
-    $('.counter').html(remainingChar);
-
-    if (remainingChar < 0) {
-     $('.counter').css('color', 'red')
-    }
-
-    if (remainingChar > 0) {
-      $('.counter').css('color', '#312e2ebf')
-    }
-  }
-
   const $form = $("form")
-
-  $('#tweet-text').on('input', charCounter);
-
   $form.on("submit", (event) => {
 
     // Prevents the default form submission
