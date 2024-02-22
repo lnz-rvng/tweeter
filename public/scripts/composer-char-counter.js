23229e23ddb7524a6a789f364f49f10e8ee0e87d
Lance <lanceirvingmanzanal@icloud.com>
2024-02-22 01:07:37 -0700

    Update the char counter function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,18 +2,12 @@ $(document).ready(() => {
 
   const charCounter = function() {
     const maxLength = 140;
-    const length = $(this).val().length;
+    const length = $(this).val().trim().length;
     const remainingChar = maxLength - length;
 
     $('.counter').html(remainingChar);
 
-    if (remainingChar < 0) {
-      $('.counter').css('color', 'red');
-    }
-
-    if (remainingChar > 0) {
-      $('.counter').css('color', '#312e2ebf');
-    }
+    $('.counter').css('color', remainingChar < 0 ? 'red' : '#312e2ebf');
   };
 
   $('#tweet-text').on('input', charCounter);
